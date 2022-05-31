d882189b4b9d6117929deaf1ec2de4e758f0bb1c
g z <gzwilly@163.com>
2022-06-01 02:12:56 +0800

    test: test

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-# _*_ encoding=utf-8 _*_
-
+#!/usr/bin/env python3
+import logging
 import os
+import time
+import logging
 
-try:
-    print(0)
-except ImportError:
-    os.system("pip3 install tweepy")
-    os.system("pip3 install tweepy[async]")
-    
-# pip3 install configparser
-# pip3 install pandas
+from lib.log import writelog
+from twitter import handle
+
+CurFilePath = os.path.abspath(__file__)
+# print("CurFilePath=%s" % CurFilePath)
+CurFilename = os.path.basename(CurFilePath)
+# 'autoSearchGame_YingYongBao.py'
+
+CurFolder = os.path.dirname(CurFilePath)
+# print("CurFolder=%s" % CurFolder)
+
+LogFolder = os.path.join(CurFolder, "logs")
+
+
+# pip3 install python-dateutil
+def initLog():
+    curDatetimeStr = time.strftime("%Y-%m-%d", time.localtime())  # '20231111'
+    os.makedirs(LogFolder, exist_ok=True)
+    curLogFile = "{}.log".format(curDatetimeStr)
+    logFullPath = os.path.join(LogFolder, curLogFile)
+    writelog.loggingInit(logFullPath)
+
+
+if __name__ == '__main__':
+    initLog()
+    twitter = handle.Handle("cats")
+    twitter.tweets()
+    print(twitter.twitters)
+    # while True:
+    #     logging.info("log info")
+    #     time.sleep(1)
--- twitterApi/twitter_api.py
+++ twitterApi/twitter_api.py
@@ -1,9 +1,22 @@
 # _*_ encoding=utf-8 _*_
 
-from ast import keyword
-import configparser
-import tweepy
-import pandas as pd
+import os
+
+try:
+    import configparser
+except :
+    os.system("pip3 install configparser")
+
+try:
+    import tweepy
+except :
+    os.system("pip3 install tweepy")
+    os.system("pip3 install tweepy[async]")
+
+try:
+    import pandas as pd
+except :
+    os.system("pip3 install pandas")
 
 # load config
 config = configparser.ConfigParser()
@@ -15,11 +28,11 @@ api_key_secret = config['twitter']['api_key_secret']
 access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
 
-print(api_key,api_key_secret,access_token,access_token_secret)
+print(api_key, api_key_secret, access_token, access_token_secret)
 
 # authentication
-auth = tweepy.OAuthHandler(api_key,api_key_secret)
-auth.set_access_token(access_token,access_token_secret)
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
@@ -27,17 +40,17 @@ api = tweepy.API(auth)
 class Linstener(tweepy.Stream):
     tweets = []
     limit = 10
+
     def on_status(self, status):
         #  super().on_status(status) 
         # print(status.user.screen_name + ': ' + status.text)
         self.tweets.append(status)
         if len(self.tweets) == self.limit:
             self.disconnect()
-     
-         
+
 
 stream_tweet = Linstener(
-     api_key,
+    api_key,
     api_key_secret,
     access_token,
     access_token_secret)
@@ -47,25 +60,22 @@ stream_tweet = Linstener(
 # stream_tweet.filter(track=keyword)
 
 # stream by user
-users = ['gzwilly1']
+users = ['sas']
 user_ids = []
 
 for user in users:
     user_ids.append(api.get_user(screen_name=user).id)
-    
+
 print(user_ids)
 exit()
 
-    
-
-columns = ["User","Tweet"]
+columns = ["User", "Tweet"]
 data = []
 for item in stream_tweet.tweets:
-    if not item.truncated :
-        data.append([item.user.screen_name,item.text])
+    if not item.truncated:
+        data.append([item.user.screen_name, item.text])
     else:
-        data.append([item.user.screen_name,item.extendeed_tweet['full_text']])
-df = pd.DataFrame(data,columns=columns)
+        data.append([item.user.screen_name, item.extendeed_tweet['full_text']])
+df = pd.DataFrame(data, columns=columns)
 
 print(df)
-
