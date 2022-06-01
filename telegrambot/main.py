4e1ee1626ced1f84acbecda55e62791578f1d4f6
g z <gzwilly@163.com>
2022-06-01 16:39:21 +0800

    test: test

--- telegrambot/main.py
+++ telegrambot/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+from cgitb import text
+from random import randint
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 
     # os.system("pip3 install aiogram")
 
@@ -13,17 +16,43 @@ class Bots:
         self.bot = Bot(token=api_key)
         dp = Dispatcher(self.bot)
         
-        @dp.message_handler(commands=['start', 'help'])
+        button_start_monitor = InlineKeyboardButton("开启监控",callback_data="start_monitor")  
+        button_stop_monitor = InlineKeyboardButton("停止监控",callback_data="stop_monitor")
+        keyboard_inline = InlineKeyboardMarkup.add(button_start_monitor).add(button_stop_monitor)
+        
+        @dp.message_handler(commands=['menu'])
         async def send_welcome(message: types.Message):
-            """当用户发送 `/start` 或 `/help` 命令时，将调用此处理程序
+            """返回控制菜单
+
             Args:
                 message (types.Message): _description_
             """
-            await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+            await message.reply("监控菜单",reply_markup=keyboard_inline)
+            
+        @dp.callback_query_handler(text=["start_monitor","stop_monitor"])
+        async def return_mune(call:types.CallbackQuery):
+            if call.data == "start_monitor":
+                await call.message.answer(randint(1,10))   
+            
+            if call.data == "stop_monitor":
+                await call.message.answer(randint(100,1000))  
+            
+            
+             
+            
+        @dp.message_handler(commands=['start'])
+        async def send_welcome(message: types.Message):
+            """当用户发送 `/start` 或 `/help` 命令时，将调用此处理程序
+            
+            Args:
+                message (types.Message): _description_
+            """
+            await message.reply("监控已准备就绪")
             
         @dp.message_handler()
         async def echo(message: types.Message):
             """处理聊天中的所有文本消息，只需添加不带过滤器的处理程序
+            
             Args:
                 message (types.Message): _description_
             """
